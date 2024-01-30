abf67cc02b93b728d5901856748e5f7d4282c6c5
Stefan Gheorghe <sgheorghe@T5PT483.AUSYNET.FR>
2024-01-30 15:22:41 +0200

    empty dev branch and move the code to feature branch

--- Utility.cpp
+++
@@ -1,9 +0,0 @@
-#include "Utility.h"
-
-void Utility::printCustom(const std::string& mesaj) {
-    std::cout << mesaj << std::endl;
-}
-
-double Utility::getAvr(double numar1, double numar2) {
-    return (numar1 + numar2) / 2.0;
-}
