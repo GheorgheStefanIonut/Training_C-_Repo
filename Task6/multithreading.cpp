8fad57ccdf02e5de9b32e136384aa6b20d2411ec
Stefan Gheorghe <sgheorghe@T5PT483.AUSYNET.FR>
2024-02-27 22:32:01 +0200

    added observer

--- Task6/multithreading.cpp
+++ Task6/multithreading.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
 
 // Extern variable
 extern int sharedNrOfCars;
@@ -8,11 +9,22 @@ extern int sharedNrOfCars;
 // Mutex for sync
 std::mutex mtx;
 
-// Task for the threads
-void task() {
+// Condition variable for observer notification
+std::condition_variable cv;
 
+// Observer class
+class Observer {
+public:
+    // Observer update method
+    void update(int newValue) {
+        std::cout << "Observer: New value of sharedNrOfCars is " << newValue << std::endl;
+    }
+};
+
+// Task for the threads
+void task(Observer& observer) {
     // Lock mutex for exclusive access to sharedNrOfCars
-    mtx.lock();
+    std::unique_lock<std::mutex> lock(mtx);
 
     int value = sharedNrOfCars;
     value++;
@@ -21,18 +33,28 @@ void task() {
     // Update the extern variable
     sharedNrOfCars = value;
 
-    // mutex unlock 
-    mtx.unlock();
+    // Notify observer
+    observer.update(value);
+
+    // Notify condition variable for observer notification
+    cv.notify_one();
 }
 
 int main() {
     sharedNrOfCars = 0;
-    std::thread thread1(task);
-    std::thread thread2(task);
 
-    // Join  threads
+    // Create observer
+    Observer observer;
+
+    std::thread thread1(task, std::ref(observer));
+    std::thread thread2(task, std::ref(observer));
+
+    // Join threads
     thread1.join();
     thread2.join();
+
+    // Print the synchronized value of sharedNrOfCars
+    std::cout << "Thread sync: " << sharedNrOfCars << std::endl;
 
     return 0;
 }
